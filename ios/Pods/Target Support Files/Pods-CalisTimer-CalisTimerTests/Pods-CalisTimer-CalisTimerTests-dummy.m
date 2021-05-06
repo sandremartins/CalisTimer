9f9101c41f6705926a311609d0bf44123b25b002
Alessandre Martins <sandremartins@gmail.com>
2021-05-06 11:05:41 -0400

    Commit init

---
+++ ios/Pods/Target Support Files/Pods-CalisTimer-CalisTimerTests/Pods-CalisTimer-CalisTimerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CalisTimer_CalisTimerTests : NSObject
+@end
+@implementation PodsDummy_Pods_CalisTimer_CalisTimerTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-CalisTimer/Pods-CalisTimer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CalisTimer : NSObject
+@end
+@implementation PodsDummy_Pods_CalisTimer
+@end
