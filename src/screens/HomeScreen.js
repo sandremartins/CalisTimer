a05cb59220a23fb4381c656c7ff122b89ba1e591
Alessandre Martins <sandremartins@gmail.com>
2021-05-09 20:55:54 -0400

    Button tela home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import Button from '../components/Button'
 
 const HomeScreen = props => {
   return(
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container} >
+      <Text style={styles.logo}>CalisTimer</Text>
+      <Button style={styles.btn} onPress={() => props.navigation.navigate('EMOM')}>EMOM</Button>
+      <Button style={styles.btn} onPress={() => props.navigation.navigate('EMOM')}>AMRAP</Button>
+      <Button style={styles.btn} onPress={() => props.navigation.navigate('EMOM')}>Isometria</Button>
     </View>
   )
 }
+HomeScreen.navigationOptions = {
+  headerShown: false
+}
+const styles = StyleSheet.create({
+  container:{
+      flex:1, 
+      backgroundColor: '#D6304A'
+  },
+  logo: {
+    fontFamily: 'Ubuntu-Bold', 
+    fontSize: 48, 
+    textAlign: 'center', 
+    color: 'white', 
+    marginTop: 111, 
+    marginBottom: 111},
+  btn: { 
+    padding: 20 
+  }
+})
 export default HomeScreen
